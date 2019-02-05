078dc314546b62ee639d7b4b915a8ed6ee2dea69
Wiktor Wajszczuk <wiktor.waj@gmail.com>
2019-02-05 19:52:08 +0100

    Fix the cp .bash_profile line

--- copy.sh
+++ copy.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 cp git-completion.bash /usr/local/bin/git-completion.bash
 cp git-prompt.sh /usr/local/bin/git-prompt.sh
-cp ./bash_profile ~/.bash_profile
+cp .bash_profile ~/.bash_profile
 source ~/.bash_profile
