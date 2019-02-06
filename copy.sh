cd61bc9b6271102f73bc34376a176b30df72234a
Wiktor Wajszczuk <wiktor.waj@gmail.com>
2019-02-06 13:39:02 +0100

    Concerning git-complete.bash git-prompt.sh It turns this files do already exist in : /Library/Developer/CommandLineTools/usr/share/git-core if you installed git so I changed that in ./bash_profile and copy.sh

--- copy.sh
+++ copy.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
-cp git-completion.bash /usr/local/bin/git-completion.bash
-cp git-prompt.sh /usr/local/bin/git-prompt.sh
+cp git-completion.bash //Library/Developer/CommandLineTools/usr/share/git-core/git-completion.bash
+cp git-prompt.sh /Library/Developer/CommandLineTools/usr/share/git-core/git-prompt.sh
 cp .bash_profile ~/.bash_profile
 source ~/.bash_profile
+
+
+# Comment to line 2 and 3 : You need to edit that script to check for existence of this files and if they don't exist then copy them to this directories!!
